24ec863596ddc0e4b6df9eeab2cfb09b75a86bcc
youkan <youkan94@gmail.com>
2022-06-07 22:59:07 +0900

    ルートのコンポーネントの調整

--- app.vue
+++ app.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
+  <div class="pt-10">
+    <AppBar>
+      <div class="text-white">youkan note</div>
+    </AppBar>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+import AppBar from "~~/components/AppBar";
+</script>
