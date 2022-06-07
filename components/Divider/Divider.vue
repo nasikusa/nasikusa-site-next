5bffad900451c0c934c0bbfdfb4c044cb35f13d1
youkan <youkan94@gmail.com>
2022-06-07 23:41:01 +0900

    Divider コンポーネントの追加

--- components/Divider/Divider.vue
+++ components/Divider/Divider.vue
@@ -1,12 +1,33 @@
-<script setup>
-  type Props = {
-    horizontal: Boolean;
-    vertical: Boolean;
-  };
+<script setup lang="ts">
+type Props = {
+  direction?: "horizontal" | "vertical";
+  class?: string;
+};
+const props = withDefaults(defineProps<Props>(), {
+  direction: "horizontal",
+});
 
-const props = defineProps<Props>();
+const classes = computed(() => {
+  return [
+    {
+      horizontal: props.direction === "horizontal",
+      vertical: props.direction === "vertical",
+    },
+    props.class,
+  ];
+});
 </script>
 
 <template>
-  <hr />
+  <hr class="border-0 border-solid border-inherit" :class="classes" />
 </template>
+
+<style scoped>
+.horizontal {
+  @apply w-full border-b;
+}
+
+.vertical {
+  @apply h-full border-r;
+}
+</style>
--- pages/model/index.vue
+++ pages/model/index.vue
@@ -1,12 +1,15 @@
 <template>
   <Container>
     hogehoge
+    <div class="h-20 w-0">
+      <Divider direction="vertical" class="border-yellow-500 my-5" />
+    </div>
     <Image src="/assets/image/ari17_1.jpg" />
   </Container>
 </template>
 
 <script setup>
 import Container from "@/components/Container";
-// import Image from "~~/components/Image";
 import Image from "@/components/Image";
+import Divider from "@/components/Divider";
 </script>
