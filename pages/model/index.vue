6a21e3852e044006c1b93311f19c477f86f448e4
youkan <youkan94@gmail.com>
2022-06-09 04:59:55 +0900

    新規コンポーネントのテスト

--- pages/model/index.vue
+++ pages/model/index.vue
@@ -5,6 +5,13 @@
       <Divider direction="vertical" class="border-yellow-500 my-5" />
     </div>
     <Image src="/assets/image/ari17_1.jpg" />
+    <Stack space="20px" direction="horizontal">
+      <div>aaaa</div>
+      <div>bbbbbb</div>
+      <div>ccccc</div>
+      <div>ddddd</div>
+    </Stack>
+    <Center class="w-40 h-40"> hogehoge </Center>
   </Container>
 </template>
 
@@ -12,4 +19,6 @@
 import Container from "@/components/Container";
 import Image from "@/components/Image";
 import Divider from "@/components/Divider";
+import Stack from "@/components/Stack";
+import Center from "@/components/Center";
 </script>
