511cc6e46c9667c9d75707d040c9f62607a1159b
youkan <youkan94@gmail.com>
2022-06-11 12:22:41 +0900

    feat : Text と Title コンポーネントの追加

---
+++ components/Text/Text.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+type Props = {
+  is?: string;
+};
+
+const props = defineProps<Props>();
+</script>
+
+<script lang="ts">
+export default {
+  inheritAttrs: true,
+};
+</script>
+
+<template>
+  <component
+    :is="props.is || 'span'"
+    class="app-text"
+    v-bind="props"
+    v-on="$listeners"
+  >
+    <slot />
+  </component>
+</template>
---
+++ components/Title/Title.vue
@@ -0,0 +1,5 @@
+<script setup>
+import Text from "@/components/Text";
+</script>
+
+<template></template>
