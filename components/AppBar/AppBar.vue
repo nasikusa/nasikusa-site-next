9bb40c0693556ebe4a5f02f3f70a022bf2e3c2d1
youkan <youkan94@gmail.com>
2022-06-11 12:23:51 +0900

    refactor : z-index の微調整

--- components/AppBar/AppBar.vue
+++ components/AppBar/AppBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-bar p-2 bg-orange-700 w-full h-12 fixed top-0 left-0">
+  <div class="app-bar z-50 p-2 bg-orange-700 w-full h-12 fixed top-0 left-0">
     <slot></slot>
   </div>
 </template>
